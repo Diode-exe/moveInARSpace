2b10fa85c38315af9415d73e79b8fc57fb14fd73
Diode-exe <DiodesCAER@protonmail.com>
2026-04-14 11:33:21 -0500

    Trim trailing whitespace

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class HandDrawing:
                 cv2.imshow('AR Space - Manual Drawing', frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
-                
+
             cap.release()
             cv2.destroyAllWindows()
 
